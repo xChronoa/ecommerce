03a33aad93d8fc4f24f73269e06f5310c0306e94
Rich Kylle Lauren Dela Cruz <richkyllegonzaga@gmail.com>
2024-05-29 00:23:54 +0800

    Updated API routes - Added user API routes - Updated products API routes (CRUD) - Updated some cart API routes.

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,19 +2,30 @@
 
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Routes for Users
+Route::prefix("/user")->group(function () {
+    Route::get("/", [UserController::class, 'view']);
+    Route::post("/add", [UserController::class, 'add']);
+    Route::put("/update/{id}", [UserController::class, 'update']);
+    Route::delete("/remove/{id}", [UserController::class, 'delete']);
+});
 
-Route::get("/products", [ProductController::class, 'productlist']);
+// Routes for Products
+Route::prefix("/products")->group(function () {
+    Route::get("/", [ProductController::class, 'view']);
+    Route::post("/add", [ProductController::class, 'add']);
+    Route::put("/update/{id}", [ProductController::class, 'update']);
+    Route::delete("/remove/{id}", [ProductController::class, 'delete']);
+});
 
+// Routes for Cart
 Route::prefix("cart")->group(function () {
-    Route::get("/", [CartController::class, 'cartlist']);
+    Route::get("/", [CartController::class, 'view']);
     Route::get("/total", [CartController::class, 'calculateCartTotal']);
-    Route::post("/add", [CartController::class, 'addProduct']);
-    Route::delete("/remove/{id}", [CartController::class, 'removeProduct']);
+    Route::post("/add", [CartController::class, 'addToCart']);
+    Route::delete("/remove/{id}", [CartController::class, 'remove']);
     Route::delete("/checkout", [CartController::class, 'checkout']);
 });
