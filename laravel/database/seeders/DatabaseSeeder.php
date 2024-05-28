23991768cb13b3a6c187a6c251be8c5f67c89267
Ferrer, Alastair <ferreralastair30@gmail.com>
2024-05-29 00:09:03 +0800

    Updated Database Seeder - Included dummy data for users.

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         Products::factory(10)->create();
+        User::factory(10)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
